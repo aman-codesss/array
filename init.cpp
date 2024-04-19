475e0fe1e746619a8b0e9e667522c72e624767a1
aman-codesss <amanpartelyt@gmail.com>
2024-04-19 13:31:30 +0545

    getting started with array

---
+++ init.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int arr[5];
+    // //populating with  same number other than 0.
+    // for(int i=0;i<5;i++){
+    // arr[i]={15};
+    // }
+    // //displaying the array elements
+    // for(int i=0;i<5;i++){
+    // cout<< arr[i]<<endl;
+    // }
+    int b[10]={3,4,7};
+    for(int i=0;i<10;i++){
+        cout<<b[i]<<endl;//the array is populated with 0 if not already populated
+    }
+
+
+}
